a07aa7df8cfc9872c137418c0b8733306a11160e
Roseskk <oleg419583@yandex.ru>
2023-10-04 18:08:51 +0300

    dev

---
+++ src/components/main/portfolio.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import arrowLeft from '../../images/main/arrow_left.svg'
+import arrowRight from '../../images/main/arrow_right.svg'
+
+const Portfolio: React.FC = () => {
+    return(
+        <section className={"portfolio"}>
+            <div className={"portfolio-top"}>
+                <div>
+                    <h2>НАШЕ <span>ПОРТФОЛИО</span></h2>
+                </div>
+                <hr />
+                <div>
+                    <button>asdas</button>
+                </div>
+            </div>
+            <div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Portfolio;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from "../components/main/about";
+import Portfolio from "../components/main/portfolio";
 
 const Main: React.FC = () => {
     return(
         <>
             <About />
+            <Portfolio />
         </>
     )
 }
